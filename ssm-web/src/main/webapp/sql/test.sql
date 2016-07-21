d054a47468405ff315b1db50d3ebc025b8ad93c3
maoxian <wjll8723@163.com>
2016-07-21 13:33:49 +0800

    初始化 SQL

---
+++ ssm-web/src/main/webapp/sql/test.sql
@@ -0,0 +1,15 @@
+
+
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  Table structure for `test`
+-- ----------------------------
+DROP TABLE IF EXISTS `test`;
+CREATE TABLE `test` (
+  `id` int(11) NOT NULL auto_increment,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+SET FOREIGN_KEY_CHECKS = 1;
