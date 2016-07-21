b1509310ef2475b2f8a4ab83ba1506a254a5034a
maoxian <wjll8723@163.com>
2016-07-21 13:50:31 +0800

    dao 增加 @Repository

--- ssm-web/src/main/java/com/ssm/test/dao/TestDao.java
+++ ssm-web/src/main/java/com/ssm/test/dao/TestDao.java
@@ -1,6 +1,7 @@
 package com.ssm.test.dao;
 
 import com.ssm.test.bean.TestBean;
+import org.springframework.stereotype.Repository;
 
 /**
  * @author saodiseng
@@ -8,6 +9,7 @@ import com.ssm.test.bean.TestBean;
  * @description {TODO}功能描述.
  * @date 16/7/21 下午1:04
  */
+@Repository
 public interface TestDao {
 
     Integer insert(TestBean tb);
